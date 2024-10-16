e6ddf46a4928eb99cb7b2525efa449325124fc4f
manamisinc2016 <161755382+manamisinc2016@users.noreply.github.com>
2024-10-15 19:47:21 -0500

    Add files via upload

---
+++ HW.py
@@ -0,0 +1,14 @@
+#1st Program
+result= (9**0.5) * 5
+print(result)
+#2nd Program
+print(9.99>9.98 and 1000 != 1000.1)
+#3rd Program
+print (2*2+2)
+print (2 * (2+2))
+print (6==8)
+#4th Program
+number= float("123.456")
+shifted_number= number * 10
+first_digit_after_decimal = int(shifted_number) % 10
+print (first_digit_after_decimal)
